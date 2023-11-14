d71262da54c03cedb729e65afe81f4087e1eeff4
Vladislav Evg <linesgreen@mail.ru>
2023-11-14 23:32:07 +0300

    add blogsRouter

--- src/routes/blog-route.ts
+++ src/routes/blog-route.ts
@@ -3,7 +3,7 @@ import {BlogRepository} from "../repositories/blog-repository";
 import {RequestWithParams} from "../types/common";
 import {BlogParams} from "../types/blog/input";
 import {authMiddleware} from "../middlewares/auth/auth-middleware";
-import {blogPostValidation} from "../validators/blogsValidator";
+import {blogIdValidation, blogPostValidation} from "../validators/blogsValidator";
 
 export const blogRoute = Router ({})
 
@@ -11,7 +11,7 @@ blogRoute.get('/', (req:Request, res:Response ) => {
     const blogs = BlogRepository.getAllBlogs()
     res.send(blogs)
 })
-blogRoute.get('/:id', authMiddleware, blogPostValidation() ,(req:RequestWithParams<BlogParams>, res:Response ) => {
+blogRoute.get('/:id', authMiddleware,blogIdValidation ,(req:RequestWithParams<BlogParams>, res:Response ) => {
     const id  = req.params.id
     const blog = BlogRepository.getBlogById(id)
     blog? res.send(blog) : res.sendStatus(404)
