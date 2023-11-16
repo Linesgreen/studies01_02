77da25992a453d745c7e01f3c85c4ced7758052c
Vladislav Evg <linesgreen@mail.ru>
2023-11-16 14:56:36 +0300

    add delete all data from posts

--- src/routes/blog-route.ts
+++ src/routes/blog-route.ts
@@ -12,7 +12,7 @@ blogRoute.get('/', (req:Request, res:Response ) => {
     const blogs : BlogType[] = BlogRepository.getAllBlogs()
     res.send(blogs)
 })
-blogRoute.get('/:id', authMiddleware,(req:RequestWithParams<BlogParams>, res:Response ) => {
+blogRoute.get('/:id',(req:RequestWithParams<BlogParams>, res:Response ) => {
     const id : string  = req.params.id
     const blog : BlogType | null = BlogRepository.getBlogById(id)
     blog? res.send(blog) : res.sendStatus(404)
