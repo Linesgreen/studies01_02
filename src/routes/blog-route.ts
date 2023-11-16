4bfeb578edf5783cf9873465dbf318dbaa64e0fa
Vladislav Evg <linesgreen@mail.ru>
2023-11-16 14:56:03 +0300

    add delete all data from posts

--- src/routes/blog-route.ts
+++ src/routes/blog-route.ts
@@ -12,20 +12,20 @@ blogRoute.get('/', (req:Request, res:Response ) => {
     const blogs : BlogType[] = BlogRepository.getAllBlogs()
     res.send(blogs)
 })
-blogRoute.get('/:id',(req:RequestWithParams<BlogParams>, res:Response ) => {
+blogRoute.get('/:id', authMiddleware,(req:RequestWithParams<BlogParams>, res:Response ) => {
     const id : string  = req.params.id
     const blog : BlogType | null = BlogRepository.getBlogById(id)
     blog? res.send(blog) : res.sendStatus(404)
 })
 
-blogRoute.post('/', blogPostValidation(),(req:RequestWithBody<BlogCreateModel>, res:Response ) => {
+blogRoute.post('/', authMiddleware,blogPostValidation(),(req:RequestWithBody<BlogCreateModel>, res:Response ) => {
     let {name, description, websiteUrl} : PostBlogReqBody = req.body;
     const newBlogId : string = BlogRepository.addBlog({name, description, websiteUrl})
     res.status(201).send(BlogRepository.getBlogById(newBlogId))
 
 })
 
-blogRoute.put('/:id',blogPutValidation(),(req: RequestWithBodyAndParams<BlogParams, BlogUdateModel>, res:Response )=> {
+blogRoute.put('/:id',authMiddleware,blogPutValidation(),(req: RequestWithBodyAndParams<BlogParams, BlogUdateModel>, res:Response )=> {
     const id : string = req.params.id;
     const {name, description, websiteUrl} : BlogUdateModel = req.body;
     const updateResult = BlogRepository.updateBlog({name, description, websiteUrl}, id)
