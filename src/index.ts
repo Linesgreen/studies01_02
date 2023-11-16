4159ac8aaf514145eb0e3196c8a06817545564fa
Vladislav Evg <linesgreen@mail.ru>
2023-11-16 14:22:10 +0300

    add Posts routs

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import express, {Request, Response} from "express";
 import {RouterPaths, videoRouter} from "./routes/videos-router";
 import {db} from "./db/db";
 import {blogRoute} from "./routes/blog-route";
+import {postRoute} from "./routes/post.route";
 
 export const app  = express()
 const port = 3002;
@@ -11,6 +12,7 @@ const port = 3002;
 app.use(express.json())
 app.use(RouterPaths.videos, videoRouter)
 app.use('/blogs', blogRoute)
+app.use('/posts', postRoute)
 
 app.get('/', (req : Request, res : Response) => {
     res.send('Заглушка')
